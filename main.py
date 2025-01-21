0cd0e8a247812a0f89d7d072f21e8f6f5979ecbf
Boss <zsuphanat.t@pttdigital.com>
2025-01-21 16:29:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from myserver import server_on
 
 import json
 from datetime import datetime, timedelta
+from pytz import timezone
 
 # Load JSON file
 with open('./100tower.json', 'r') as file:
@@ -134,7 +135,7 @@ async def costumecommand(interaction):
 
 
 def hours_between_Today(d1):
-    d2 = datetime.now()
+    d2 = datetime.now(tzinfo=timezone('UCT'))
     return (d2 - d1).total_seconds()/3600
 
 # /////////// END Func ////////////// 
